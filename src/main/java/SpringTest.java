ef36b0b43ace67a659cf3cb32dfb4db59a0b03fa
jiangdongzi <jiangdongzidi@126.com>
2018-09-11 22:25:57 +0800

    modified:   src/main/java/SpringTest.java new file:   src/main/java/TestBean.java modified:   src/main/resources/applicationContext.xml used2beTest

--- src/main/java/SpringTest.java
+++ src/main/java/SpringTest.java
@@ -1,9 +1,21 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.SQLOutput;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value="classpath:applicationContext.xml")
 public class SpringTest {
-    
+
+    @Autowired
+    private TestBean testBean;
+    @Test
+    public void test()
+    {
+        System.out.println(testBean.getId());
+        System.out.println(testBean.getName());
+    }
 }
