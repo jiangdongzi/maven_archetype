beba6c7538e0e16b09f549e86f81f05caca2d228
liuzhenguo <liuzhenguo@xiaomi.com>
2019-09-09 16:37:00 +0800

    add resource cfg

--- src/main/java/com/jedisClusterTest/StagingRedisClusterTest.java
+++ src/main/java/com/jedisClusterTest/StagingRedisClusterTest.java
@@ -1,23 +1,15 @@
 package com.jedisClusterTest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import redis.clients.jedis.JedisCluster;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value="classpath:applicationContext.xml")
 public class StagingRedisClusterTest {
 
-    @Autowired
-    JedisCluster jedisCluster;
 
-    @Test
-    public void testJedis() {
-        jedisCluster.setex("cached-userId-by-outId-1-2201852814", 1000, "hello");
-        System.out.println(jedisCluster.get("cached-userId-by-outId-1-2201852814"));
-        System.out.println(jedisCluster.exists("cached-userId-by-outId-1-2201852814"));
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JedisCluster jed = ctx.getBean("jedisCluster", JedisCluster.class);
+        String s = jed.get("cached-userId-by-outId-1-2201852814");
+        System.out.println(s);
     }
 }
